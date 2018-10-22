af0d00e11759aef0ef998a16fb7466547c854eec
Ravikudidela <kudidelaravi@gmail.com>
2018-10-22 22:55:48 +0530

    added ndfor to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstApp';
+  title = 'first';
+  public name = 'Ravi';
+  public showname: any[] = [];
+  public list: any[] = ['1','2','3','4'];
 }
